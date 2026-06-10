361e37e17cd4c5629eec60b86693640ac38e6646
pradhumn2102 <p7895860813@gmail.com>
2026-06-11 04:11:59 +0530

    Day_7

---
+++ Day_7/Q27.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Recursive function to find sum of digits
+int sumDigits(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return (n % 10) + sumDigits(n / 10);
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Sum of digits = %d\n", sumDigits(num));
+
+    return 0;
+}
