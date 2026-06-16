bc20c7e66eb22fd581553d742e02d79dc6eac8b8
pradhumn2102 <p7895860813@gmail.com>
2026-06-16 17:03:48 +0530

    Day_18

---
+++ Day_18/Q71.c
@@ -0,0 +1,42 @@
+   #include <stdio.h>
+
+int main() {
+    int arr[100], n, i, key;
+    int low, high, mid, found = 0;
+
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d sorted elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            found = 1;
+            printf("Element found at position %d", mid + 1);
+            break;
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if(found == 0) {
+        printf("Element not found");
+    }
+
+    return 0;
+}
